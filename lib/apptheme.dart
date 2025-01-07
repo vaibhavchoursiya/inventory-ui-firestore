91ab7f9189426c3015f545c0bb02f595f3b3773e
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-07 13:27:40 +0530

    initial commit

---
+++ lib/apptheme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Apptheme {
+  static const Color primary = Color(0xFF00B251);
+  static const Color dark = Colors.black87;
+  static const Color light = Color(0xFFffffff);
+}
