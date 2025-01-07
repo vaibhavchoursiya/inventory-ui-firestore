ab85d62c69ee71e326c3d9447af4d06160364fdc
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-07 17:57:26 +0530

    hero section is added.

--- lib/apptheme.dart
+++ lib/apptheme.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class Apptheme {
   static const Color primary = Color(0xFF00B251);
-  static const Color dark = Color(0xFF474747);
+  static const Color dark = Color.fromARGB(255, 10, 10, 10);
   static const Color light = Color(0xFFffffff);
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:inventory_management_app/apptheme.dart';
 import 'package:inventory_management_app/widgets/hero_section.dart';
 import 'package:inventory_management_app/widgets/label_header_widget.dart';
 import 'package:inventory_management_app/widgets/navbar_widget.dart';
+import 'package:inventory_management_app/widgets/product_grid.dart';
 import 'package:inventory_management_app/widgets/search_filter_widget.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -13,27 +14,30 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Apptheme.light,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(28.0),
-          child: Column(
-            children: [
-              const NavBarWidget(),
-              const SizedBox(
-                height: 25.0,
-              ),
-              const SearchFilterWidget(),
-              const SizedBox(
-                height: 30.0,
-              ),
-              const HeroSection(),
-              const SizedBox(
-                height: 30.0,
-              ),
-              LabelHeaderWidget(
-                label: "Featured Products",
-                function: () {},
-              ),
-            ],
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(28.0),
+            child: Column(
+              children: [
+                const NavBarWidget(),
+                const SizedBox(
+                  height: 25.0,
+                ),
+                const SearchFilterWidget(),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                const HeroSection(),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                LabelHeaderWidget(
+                  label: "Featured Products",
+                  function: () {},
+                ),
+                ProductGrid()
+              ],
+            ),
           ),
         ),
       ),
