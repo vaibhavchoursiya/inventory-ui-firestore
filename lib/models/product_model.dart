6f74e36d370787fb69b6276e59cb4dd1fb6c3112
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-07 13:32:04 +0530

    product model created.

---
+++ lib/models/product_model.dart
@@ -0,0 +1,12 @@
+class ProductModel {
+  final String name;
+  final String description;
+  final int totalQuantity;
+  final double price;
+
+  ProductModel(
+      {required this.name,
+      required this.description,
+      required this.totalQuantity,
+      required this.price});
+}
