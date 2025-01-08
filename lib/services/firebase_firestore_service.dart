6be6931c8c2b3939bf049635eca91b311cb7da7a
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-08 09:33:14 +0530

    add and update product is added.

--- lib/services/firebase_firestore_service.dart
+++ lib/services/firebase_firestore_service.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:inventory_management_app/models/product_model.dart';
 
 class FirebaseFirestoreService {
   static final FirebaseFirestore firestore = FirebaseFirestore.instance;
@@ -17,7 +18,23 @@ class FirebaseFirestoreService {
   }
 
   static Future getDocumentById(String id) async {
-    final document = products.doc(id).get();
+    final document = await products.doc(id).get();
     return document;
+  }
+
+  static Future<void> addProduct(ProductModel model) async {
+    await products.add(model.toMap());
+  }
+
+  static Future<void> updateProductById(String id, ProductModel model) async {
+    final product = await getDocumentById(id);
+    final data = {
+      "name": model.name,
+      "description": model.description,
+      "totalQuantity": product["totalQuantity"] - model.totalQuantity,
+      "price": model.price
+    };
+
+    await products.doc(id).update(data);
   }
 }
