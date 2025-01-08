96aeaa6956365dae7e130223cb108399ea2095ab
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-08 09:03:23 +0530

    product model is updated.

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,12 +1,35 @@
 class ProductModel {
+  final String id;
   final String name;
   final String description;
   final int totalQuantity;
   final double price;
+  bool isAddToCart;
 
   ProductModel(
-      {required this.name,
+      {required this.id,
+      required this.name,
       required this.description,
       required this.totalQuantity,
-      required this.price});
+      required this.price,
+      this.isAddToCart = false});
+
+  toMap() {
+    return {
+      "name": name,
+      "description": description,
+      "totalQuantity": totalQuantity,
+      "price": price,
+    };
+  }
+
+  factory ProductModel.fromJson(Map model) {
+    return ProductModel(
+        id: model["id"],
+        name: model["name"],
+        description: model["description"],
+        totalQuantity: model["totalQuantity"],
+        price: model["price"],
+        isAddToCart: false);
+  }
 }
