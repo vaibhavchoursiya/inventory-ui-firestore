d6baf46cec15305d57736ee8f2e49e0244c226de
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-01-08 09:21:32 +0530

    getProductStream and getProducts is created.

---
+++ lib/services/firebase_firestore_service.dart
@@ -0,0 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirebaseFirestoreService {
+  static final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  static final CollectionReference products = firestore.collection("products");
+  static final CollectionReference orders = firestore.collection("orders");
+
+  static Stream<QuerySnapshot> getProductStream() {
+    final snapshot = products.snapshots();
+    return snapshot;
+  }
+
+  static Future getProducts() async {
+    final snapshot = await products.get();
+    final finalData = snapshot.docs.map((doc) => doc.data()).toList();
+    return finalData;
+  }
+}
